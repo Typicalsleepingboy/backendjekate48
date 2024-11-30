3dc13e1f8f14a56a425e98cc255f823a45954bc6
typicalsleepingboy <mpluto772@gmail.com>
2024-12-01 01:35:15 +0700

    add

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const connectDB = require('./config/database');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const cors = require('cors');
+
+
+const app = express();
+connectDB();
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "VANGTECH Telkom University",
+        author: "https://github.com/vangtechv1",
+    });
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
