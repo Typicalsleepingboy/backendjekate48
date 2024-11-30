03364ad39b980db699995e18b3035c4d5a23e040
typicalsleepingboy <mpluto772@gmail.com>
2024-12-01 01:45:28 +0700

    fix cors

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const cors = require('cors');
 
-
 const app = express();
+
 connectDB();
-app.use(express.json());
-app.use(cors({ origin: '*' }));
+
+app.use(express.json());  
+app.use(cors({ origin: '*' }));  
+
+app.options('*', cors());
 
 app.get("/", (req, res) => {
     res.send({
-        message: "VANGTECH Telkom University",
-        author: "https://github.com/vangtechv1",
+        message: "Apa pantek???",
+        author: "https://github.com/typicalsleeping",
     });
 });
 
